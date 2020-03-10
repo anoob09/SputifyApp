4fc0698035d4968a6835222cf1d8b68421ce6e46
anoob09 <aswarnkar.anup.swarnkar@gmail.com>
2020-03-11 03:29:55 +0530

    Added current locatin feature

--- app/src/main/java/com/example/sputify/MainActivity.java
+++ app/src/main/java/com/example/sputify/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.sputify;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationRequest;
@@ -31,24 +39,62 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Location
+    private FusedLocationProviderClient fusedLocationClient;
+    private double mLattitude = 0;
+    private double mLongitude = 0;
+
+
+    //Spotify Auth
     public static final String CLIENT_ID = "089d841ccc194c10a77afad9e1c11d54";
     public static final String REDIRECT_URI = "spotify-sdk://auth";
     public static final int AUTH_TOKEN_REQUEST_CODE = 0x10;
     public static final int AUTH_CODE_REQUEST_CODE = 0x11;
 
+    //okhttp
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private String mAccessToken;
     private String mAccessCode;
     private Call mCall;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        boolean permissionAccessCoarseLocationApproved =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED;
+        if(permissionAccessCoarseLocationApproved)
+            Toast.makeText(getApplicationContext(),"Permission Approved",Toast.LENGTH_SHORT).show();
+        else
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    69);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            mLattitude = location.getLatitude();
+                            mLongitude = location.getLongitude();
+                        }
+                        else{
+                            System.out.println("Location is null");
+                        }
+                    }
+                });
+
+
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle(String.format(
                 Locale.US, "Spotify Auth Sample %s", com.spotify.sdk.android.auth.BuildConfig.VERSION_NAME));
     }
+
+
 
     @Override
     protected void onDestroy() {
@@ -172,15 +218,16 @@ public class MainActivity extends AppCompatActivity {
         try {
             postdata.put("username", name);
             postdata.put("token", token);
+            postdata.put("latitude", mLattitude);
+            postdata.put("longitude", mLongitude);
         } catch(JSONException e){
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        Log.wtf("post message", "message sent " + token);
         RequestBody body = RequestBody.create(MEDIA_TYPE, postdata.toString());
 
         Request requestToSputifyServer = new Request.Builder()
-                .url("http://192.168.0.102:4000/")
+                .url("http://192.168.0.102:5000/")
                 .post(body)
                 .header("Accept", "application/json")
                 .header("Content-Type", "application/json")
